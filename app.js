7de73a5203c9c9f96d598bc2f88a6ed582718f3c
Taofeek <ibrahimtaofeek005@gmail.com>
2025-10-05 09:39:06 +0100

    changed index to app

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
     // app.js
     const express = require('express');
     const swaggerUi = require('swagger-ui-express');
-    const messageRoutes = require('./routes/authRoutes.js');   
-
-    // const YAML = require('yamljs');
-    // const path = require('path');
-    const swaggerSpec = require('./swagger.js');
+    const swaggerDocument = require('./swagger.js');
+    const messageRoutes = require('./routes/authRoutes.js');
+    const YAML = require('yamljs');
+    const path = require('path');
 
     const app = express();
     const port = process.env.PORT || 3000;
@@ -13,14 +12,15 @@
     // Load Swagger document
     // const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
-    // Serve Swagger UI
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
     app.use('/api/auth', messageRoutes);
+
+
+    // Serve Swagger UI
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     // Define your API routes (example)
     app.get('/', (req, res) => {
-        res.json([ message: "Welcome to Swagger Vercel!"]);
+        res.json([{  message: 'Welcome to the API!' }]);
     });
 
     app.listen(port, () => {
